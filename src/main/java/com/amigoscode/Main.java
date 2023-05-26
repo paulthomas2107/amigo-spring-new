8075885c23512e2bc139f0e6e80bccac288064cd
Paul Thomas <paulthomas2107@gmail.com>
2023-05-26 14:27:37 +0100

    More

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -1,25 +1,22 @@
 package com.amigoscode;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@SpringBootApplication
-@ComponentScan(basePackages = "com.amigoscode")
-@EnableAutoConfiguration
-@Configuration
+@SpringBootApplication
 @RestController
+
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
     @GetMapping("/greet")
-    public String greet() {
-        return "Hello";
+    public GreetResponse greet() {
+        return new GreetResponse("Hi...");
     }
+
+    record GreetResponse(String greet) {}
 }
