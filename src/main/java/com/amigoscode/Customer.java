a831db277bdf680120a8037da888f337455294f5
Paul Thomas <paulthomas2107@gmail.com>
2023-05-26 17:27:07 +0100

    More

--- src/main/java/com/amigoscode/Customer.java
+++ src/main/java/com/amigoscode/Customer.java
@@ -1,12 +1,24 @@
 package com.amigoscode;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Entity
 public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence",
+            allocationSize = 1
+    )
 
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String name;
     private String email;
