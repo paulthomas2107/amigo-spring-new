3a81ed9646b27ad6aa018a2b4b0180ae0a660fda
Paul Thomas <paulthomas2107@gmail.com>
2023-05-26 18:09:50 +0100

    More

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -2,9 +2,7 @@ package com.amigoscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,29 @@ public class Main {
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
     }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ) {
+
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
+
 
     @GetMapping("/greet")
     public GreetResponse greet() {
